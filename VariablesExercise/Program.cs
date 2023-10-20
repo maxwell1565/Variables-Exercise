2ab1d723328a1354d7032ee64a7490bfc5644165
maxwell1565 <146258976+maxwell1565@users.noreply.github.com>
2023-10-20 17:29:08 -0500

    initialcommit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string goldfishName = "Steve";
+            int goldfishAge = 1;
+            char firstInitial = 'S';
+            bool isSmart = true;
+            double hisTricks = 2;
+            decimal newTricks = 0.05m;
+
+            Console.WriteLine($"My goldfish's name is {goldfishName}. His first initial is {firstInitial}. " +
+                $"Steve is {goldfishAge} year old.");
+            Console.WriteLine($"It is {isSmart} that Steve is smart.");
+            Console.WriteLine($"He knows {hisTricks} tricks and is learning {newTricks} more.");
+            
         }
     }
 }
